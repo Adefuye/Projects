e06d696f769d88340906025385afb956678af823
EMAXthedestroyer <emmanuel.adefuye74@gmail.com>
2019-04-01 12:30:11 -0500

    Lab_8 Fraction and Tester

--- Tester.java
+++ Tester.java
@@ -2,16 +2,23 @@ public class Tester
 {
 	public static void main(String[] args)
 	{
-		String[] arr = {"a","b","c"};
-		//ArrayUtil.reverse(arr);
-		arr = ArrayUtil.add("d",arr);
-		for(int i=0; i<arr.length;i++)
-		{
-			System.out.print(arr[i]+" ");
-		}
-		//arr = ArrayUtil.resize(arr);
-		
-		System.out.print("\n"+arr.length);
-	
+		test(1,2,1,3,"+");
+		test(1,4,3,2,"-");
+		test(1,4,3,2,"*");
+		test(1,4,3,2,"/");
 	}
+public static void test(int n1, int d1, int n2, int d2, String op)
+{
+	Fraction f1 = new Fraction(n1, d1);
+	Fraction f2 = new Fraction(n2, d2);
+	Fraction result = null;
+	switch(op)
+	{
+		case "+": { result = f1.add(f2); }break;
+		case "-": { result = f1.subtract(f2); }break;
+		case "*": { result = f1.multiply(f2); }break;
+		case "/": { result = f1.divide(f2); }break;
+	}
+	System.out.printf("%s%s%s=%s\n",f1,op,f2,result);
+}
 }
