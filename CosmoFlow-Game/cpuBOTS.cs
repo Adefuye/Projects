8b8fb20bf47212ee0576c892a15411555b5c3cbc
eaadefuy <eaadefuy@uno.edu>
2019-12-13 01:17:36 -0600

    CPU Bots and inGamTimer Added

---
+++ CosmoFlow-Game/cpuBOTS.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class cpuBOTS : MonoBehaviour
+{
+    Transform target;
+    private GameObject finishLine;
+    private float speed;
+    private float nextWaypointDistance;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        speed = 8000f;
+        nextWaypointDistance = 100f;
+        finishLine = GameObject.FindWithTag("botPlayers");
+        target = finishLine.transform;
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("updatePath", 0f, .5f);
+
+    }
+    void updatePath()
+    {
+        if(seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //rb.velocity = new Vector2(40, rb.velocity.y);
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        
+
+        if(path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
---
+++ CosmoFlow-Game/inGameTimer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class inGameTimer : MonoBehaviour
+{
+    public float seconds = 0;
+    public int minutes = 0;
+    public GameManager timeManager;
+    public Text timer;
+    public Text endScreenTimer;
+    //public Text endTime;
+    public void totalTime()
+    {    
+        if(seconds>59.999){minutes+=1; seconds = 0;}
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            //timeManager.gameHasEnded = false;
+            
+            if(timeManager.gameHasEnded == false)
+            {
+                seconds += Time.deltaTime;
+                timer.text = minutes+":"+seconds.ToString("0");//inGame realtime timer
+                totalTime();
+                //Debug.Log(seconds +" "+timeManager.gameHasEnded);
+            }
+            if(timeManager.gameHasEnded == true)
+            {
+                seconds = (int)seconds;
+                minutes = (int)minutes;
+                endScreenStats();
+                resetTimer();
+                //Debug.Log(seconds +" "+timeManager.gameHasEnded);
+                timeManager.gameHasEnded = false;
+            }
+        }
+        else
+        {
+            resetTimer();
+        }
+        
+        
+    }
+    void endScreenStats()
+    {
+        //timer.text = minutes+":"+seconds.ToString("0");//to string trunates the decimals
+        endScreenTimer.text = timer.text;
+    }
+    public void resetTimer()
+    {
+        seconds = 0;
+        minutes = 0;
+    }
+}
