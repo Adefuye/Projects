f0369730d44af00d3bec4d7d4e44b785163efc75
VyberZ <74570926+VyberZ@users.noreply.github.com>
2020-11-16 22:13:21 +0000

    Update bot.py

--- PythonViewBot/bot.py
+++ PythonViewBot/bot.py
@@ -5,10 +5,10 @@ from selenium import webdriver;
 Timer = 120
 
 #youtube link
-link = 'https://www.youtube.com/watch?v=hW_WFUs3hfQ'
+link = 'https://www.youtube.com/channel/UCQo341lE6vuqYIIzHX3F6eg'
 
 #number of views
-views = 20
+views = 1000
 
 driver = webdriver.Chrome()
 driver.get(link)
