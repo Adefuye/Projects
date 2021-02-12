ebe741d4238c547926e242a5416d1f65f45e8e63
Emmanuel Adefuye <emmanuel.adefuye74@gmail.com>
2021-02-12 14:08:24 -0600

    Sample Project Site layout

---
+++ ReactJS/test-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <ul className="navList">
+                <li>PROJECTS</li>
+                <li>CONTACT</li>
+			</ul>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ ReactJS/test-app/src/components/ProjectCards.js
@@ -0,0 +1,15 @@
+const ProjectCards = () => {
+    return(
+        <div className = "projectCards">
+            <h1>
+                Name of Project
+            </h1>
+
+            <p>
+                Description details!
+            </p>
+        </div>
+    )
+}
+
+export default ProjectCards
