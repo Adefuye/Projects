760cf500d5acac90a2e9637d40694e967cad175f
Emmanuel Adefuye <emmanuel.adefuye74@gmail.com>
2021-02-15 17:40:09 -0600

    MVP Layout

--- ReactJS/test-app/src/components/CollectionCard.js
+++ ReactJS/test-app/src/components/CollectionCard.js
@@ -1,14 +1,15 @@
 const CollectionCard = () => {
     return(
         <div className = "collectionCards">
+            <div id="gradient">
+                <h1>
+                    Collection Card Title
+                </h1>
 
-            <h1>
-                Collection Card Title
-            </h1>
-
-            <p>
-                Card Description details!
-            </p>
+                <p>
+                    Card Description details!
+                </p>
+            </div>
         </div>
     )
 }
