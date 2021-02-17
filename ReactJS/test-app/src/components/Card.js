c18c38cf3702800d4af6c33b46500478e185f07a
Emmanuel Adefuye <emmanuel.adefuye74@gmail.com>
2021-02-16 21:25:44 -0600

    Functionality: Adding Cards to collection

--- ReactJS/test-app/src/components/Card.js
+++ ReactJS/test-app/src/components/Card.js
@@ -1,7 +1,12 @@
+import { FaTimes } from 'react-icons/fa'
+
 const Card = ({card}) =>{
     return(
         <div className="card">
-            <h3>{card.title}</h3>
+             <FaTimes className="xBtn"/>
+            <h3>
+                {card.title}
+            </h3>
             <p>{card.description}</p>
         </div>
     )
