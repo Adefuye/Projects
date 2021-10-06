0b88d8b9e04af0bb2868637dc063fcac22eeae22
Emmanuel Adefuye <emmanuel.adefuye74@gmail.com>
2021-10-05 22:33:02 -0500

    ABle to connect and display client's name on server

--- Java Socket Programming/client.java
+++ Java Socket Programming/client.java
@@ -10,5 +10,16 @@ public class client{
     public static void main(String[] args) throws IOException
     {
         Socket s = new Socket("localhost", 4999);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name: ");
+        String username = input.nextLine();
+        System.out.println("message: ");
+        String message = input.nextLine();
+
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println(username);
+        pr.flush();
     }
 }
--- Java Socket Programming/server.java
+++ Java Socket Programming/server.java
@@ -12,6 +12,12 @@ public class server{
         ServerSocket ss = new ServerSocket(4999);
         Socket s = ss.accept();
 
-        System.out.println("Client Connected");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println(str + " Connected");
+        System.out.println(str + ": [message goes here]" );
     }
 }
